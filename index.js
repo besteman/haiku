7d2a5be519a69e5c7e99791a95e32badbfa43d45
besteman <ellioy37@gmail.com>
2020-07-26 17:55:40 -0400

    First pass

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 const axios = require("axios");
+const fs = require("fs"); 
+const Haiku = require("./construct_object");
 
 require('dotenv').config();
 
-const firstLine = "Hello There dude"
-
-const splitLine = firstLine.split(" ")
-
-console.log(splitLine);
-
-for(let i = 0; i < splitLine.length; i = i + 1) {
-
-    axios({
-        "method": "GET",
-        "url": `https://wordsapiv1.p.rapidapi.com/words/${splitLine[i]}/syllables`,
-        "headers": {
-        "content-type": "application/octet-stream",
-        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-        "x-rapidapi-key": process.env.WORD_API_KEY,
-        "useQueryString": true
-        }
-        })
-        .then((response)=>{
-          console.log(response.data.syllables.count)
-        })
-        .catch((error)=>{
-          console.log(error)
+const data = fs.readFileSync(
+    "./test_file.txt", 
+    {
+        encoding: "utf8", 
+        flag: "r"
     })
+    .split("\n");
 
+const haiku = new Haiku(data)
+
+console.log(haiku);
+
+const keys = Object.keys(haiku)
+
+console.log(keys);
+
+for (let i = 0; i < keys.length; i = i + 1) {
+    // console.log(haiku[keys[i]]);
+    for (let j = 0; j < haiku[keys[i]].splitLine.length; j = j + 1) {
+        console.log(haiku[keys[i]].splitLine[j]);
+        axios({
+            "method": "GET",
+            "url": `https://wordsapiv1.p.rapidapi.com/words/${haiku[keys[i]].splitLine[j]}/syllables`,
+            "headers": {
+            "content-type": "application/octet-stream",
+            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+            "x-rapidapi-key": process.env.WORD_API_KEY,
+            "useQueryString": true
+            }
+            })
+            .then((response) => {
+                console.log(response.data);
+                haiku[keys[i]].syllablesCount += response.data.syllables.count;
+
+            })
+            .catch((error) => {
+              console.log(error)
+        })
+    }
 }
+
+console.log(haiku);

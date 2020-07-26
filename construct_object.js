9b94510743a6ac41d843e20cc229bc6a3dd9f85f
besteman <ellioy37@gmail.com>
2020-07-26 17:55:27 -0400

    finish construct object for haiku

--- construct_object.js
+++ construct_object.js
@@ -1,27 +1,15 @@
-const fs = require("fs"); 
-
-const data = fs.readFileSync(
-    "./test_file.txt", 
-    {
-        encoding: "utf8", 
-        flag: "r"
-    })
-    .split("\n");
-
-
-
-export function Haiku(data){
+module.exports = function Haiku(data){
 
     this.firstLine = {};
     this.firstLine.line = data[0];
-    this.firstLine.split = data[0].split(" ");
+    this.firstLine.splitLine = data[0].split(" ");
 
     this.secondLine = {};
     this.secondLine.line = data[1];
-    this.secondLine.split = data[1].split(" ");
+    this.secondLine.splitLine = data[1].split(" ");
 
     this.thirdLine = {}
     this.thirdLine.line = data[2];
-    this.thirdLine.split = data[2].split(" ");
+    this.thirdLine.splitLine = data[2].split(" ");
 
 }
